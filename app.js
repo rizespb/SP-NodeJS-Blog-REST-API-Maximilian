752fb29882f3c114535fbcee8d14ae7fcc97e9bc
Маклачков Александр <aleksandr.maklachkov@gazprombank.ru>
2022-11-05 13:50:44 +0300

    CORS

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const feedRoutes = require('./routes/feed')
 
 const app = express()
+
+// Для парсинга данных из форм
+// app.use(bodyParser.urlencoded()) // x-www-form-urlencoded
+
+// Для парсинга json-данных
+app.use(bodyParser.json())
+
+// Настройка CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+  // Клиент может отсылать, помимо стандартных заголовков, еще заголовки 'Content-Type, Authorization'
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+
+  next()
+})
 
 app.use('/feed', feedRoutes)
 
