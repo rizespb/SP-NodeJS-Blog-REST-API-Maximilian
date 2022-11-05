3ef6d5f4664621644266bdf7276d102548d08c53
Маклачков Александр <aleksandr.maklachkov@gazprombank.ru>
2022-11-05 13:50:54 +0300

    getPosts and createPost

--- routes/feed.js
+++ routes/feed.js
@@ -4,7 +4,10 @@ const feedController = require('../controllers/feed')
 
 const router = express.Router()
 
-// Получить все посты
+// Получить все посты GET /feed/posts
 router.get('/posts', feedController.getPosts)
+
+// Создать пост POST /feed/post
+router.post('/post', feedController.createPost)
 
 module.exports = router
