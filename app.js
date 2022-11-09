3ddfdbee3f90040de84ae7935117cc93aef7d835
Маклачков Александр <aleksandr.maklachkov@gazprombank.ru>
2022-11-09 08:59:34 +0300

    Валидация данных при регистрации

--- app.js
+++ app.js
@@ -70,8 +70,11 @@ app.use((error, req, res, next) => {
 
   const status = error.statusCode || 500
   const message = error.message
+  const data = error.data
+
   res.status(status).json({
     message: message,
+    data: data,
   })
 })
 
